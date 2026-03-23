abe8bec58f82336fac6b57406185f858a85f069f
hdau620 <jacksondau95@gmail.com>
2026-03-23 13:13:52 +1300

    Update pydantic schemas so it can work with the new database model

--- schemas.py
+++ schemas.py
@@ -1,13 +1,36 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from datetime import datetime
+
+class UserBase(BaseModel):
+    pass
+    username: str = Field(min_length=1, max_length=100)
+    email: EmailStr  = Field(max_length=200)
+
+# What we expected form uer
+class UserCreate(UserBase):
+    pass
+
+# what we respond back to user
+class UserResponse(UserBase):
+    pass
+    # This allows pydantic to read from sqlalchemy model
+    model_config = ConfigDict(from_attributes=True)
+
+    id:int
+    image_file: str|None
+    image_path: str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1, max_length=100)
-    author: str = Field(min_length=1, max_length=100)
 
 class PostCreate(PostBase):
-    pass
+    user_id: int
 
 class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    # Why is it working? sending nested json back to user
+    author: UserResponse
