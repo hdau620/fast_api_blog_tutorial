3e04469d41339ece8069fb587b2ec9c80031d003
hdau620 <jacksondau95@gmail.com>
2026-03-29 07:43:33 +1300

    adding some JS for delete and edit posts

--- main.py
+++ main.py
@@ -44,7 +44,9 @@ app.include_router(posts.router, prefix="/api/posts", tags=["posts"])
 async def home(request: Request, db: Annotated[AsyncSession, Depends(get_db)]):
     #note: because there's relationship between post and user/author.So i must load eager
     result = await db.execute(
-        select(models.Post).options(selectinload(models.Post.author))
+        select(models.Post)
+        .options(selectinload(models.Post.author))
+        .order_by(models.Post.date_posted.desc())
     )
 
     posts = result.scalars().all()
@@ -146,7 +148,8 @@ async def user_posts_page(
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
-        .where(models.Post.user_id == user_id),
+        .where(models.Post.user_id == user_id)
+        .order_by(models.Post.date_posted.desc()),
     )
     posts = result.scalars().all()
     return templates.TemplateResponse(
