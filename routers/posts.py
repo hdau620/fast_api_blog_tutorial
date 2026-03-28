4c783421aaecb3328a98138e904cdfcfb9d2077c
hdau620 <jacksondau95@gmail.com>
2026-03-29 07:42:55 +1300

    orginsing routes into users and posts

--- routers/posts.py
+++ routers/posts.py
@@ -18,6 +18,7 @@ async def get_posts(db: Annotated[AsyncSession, Depends(get_db)]):
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
+        .order_by(models.Post.date_posted.desc())
     )
     posts = result.scalars().all()
     return posts
